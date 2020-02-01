7ec77b7158882eb2ad4693b50e2d0a99efc565b2
Cristian Suarez <cristian.suarez0322@gmail.com>
2020-01-31 23:53:07 -0500

    headerDone

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import gravatar from "../utils/gravatar";
 import { connect } from "react-redux";
+import { logoutRequest } from "../actions/index";
 import "../styles/Header.scss";
 import logo from "../assets/logo-platzi-video-BW2.png";
 import userIcon from "../assets/user-icon.png";
 
-
 const Header = props => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+  const handleLogo = () => {
+    props.logoutRequest({})
+  };
   return (
     <header className="header">
       <Link to="/">
@@ -17,19 +20,30 @@ const Header = props => {
       </Link>
       <div className="header__menu">
         <div className="header__menu--profile">
-          {hasUser ? 
-          <img src={gravatar(user.email)} alt={user.email} /> :
-          <img src={userIcon} alt=""></img>
-          }
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt=""></img>
+          )}
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href="/">Cuenta</a>
-          </li>
-          <li>
-            <Link to="/login">Cerrar Sesión</Link>
-          </li>
+          {hasUser ? 
+            <li>
+              <a href="/">{user.name}</a>
+            </li>
+           : null}
+          {hasUser ? 
+            <li>
+              <a href="#logout" onClick={handleLogo}>
+                Cerrar Sesión
+              </a>
+            </li>
+           : 
+            <li>
+              <Link to="/login">Iniciar Sesión</Link>
+            </li>
+          }
         </ul>
       </div>
     </header>
@@ -41,5 +55,8 @@ const mapStateToProps = state => {
     user: state.user
   };
 };
+const marpDispatchToProps = {
+  logoutRequest
+};
 
-export default connect(mapStateToProps, null)(Header);
+export default connect(mapStateToProps, marpDispatchToProps)(Header);
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Register.scss";
 
-const Register = () => (
-  <section className="register">
-    <section className="register__container">
-      <h2>Regístrate</h2>
-      <form className="register__container--form">
-        <input className="input" type="text" placeholder="Nombre" />
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
-        <button className="button">Registrarme</button>
-      </form>
-      <Link className="link" to="/login">
-        Iniciar sesión
-      </Link>
+const Register = () => {
+  const [form, setValue] = useState({
+    email: "",
+    name: "",
+    password: ""
+  });
+  // funcion con la cual escucha los cambios de los inputs, los agrega al estado con el set y se idenfica el campo por medio el atributo name 
+  const handleInput = event => {
+    setValue({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form)
+  }
+  return (
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input name="name" className="input" type="text" placeholder="Nombre" onChange={handleInput} />
+          <input name="email" className="input" type="text" placeholder="Correo" onChange={handleInput} />
+          <input name="password" className="input" type="password" placeholder="Contraseña" onChange={handleInput} />
+          <button className="button">Registrarme</button>
+        </form>
+        <Link className="link" to="/login">
+          Iniciar sesión
+        </Link>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Register;
